cf1dc4556909ea8478edecb04ed265033c4de629
GenaVinokurov <genabrest97@gmail.com>
2021-03-09 16:46:21 +0300

    feat: add final version for this task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n)
+    let array
+    let result 
+    array = Array.from(String(n), Number)
+    result = array.reverse().join('')
+    result = ++ result - 1
+    return result
 }
